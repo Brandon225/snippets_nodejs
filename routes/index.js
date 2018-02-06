25317e8a894709c5fa4305278bf98eb16c626b50
Brandon Tindle <bjtactor@gmail.com>
2018-02-05 20:33:19 -0500

    Added About and Details to project

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const router = express.Router();
 
 const path = 'http://localhost:3000'
 
+// create constants
+const date = new Date();
+const year = date.getFullYear();
 
 router.get('/', (req, res, next) => {
 
-    const date = new Date();
-    const year = date.getFullYear();
+    // create constants
     const code = `<snippet><content><![CDATA[
 console.log('$1: ', $2);
 ]]></content>
@@ -21,11 +23,11 @@ console.log('$1: ', $2);
 });
 
 router.get('/details', (req, res, next) => {
-    return res.send('Details!');
+    return res.render('details', {title: 'Details', canonical: `${path}details`, year: year, bgColor: '#ffffff', scroll: 'scroll', scrollTarget: '#sidebar-content'});
 });
 
 router.get('/about', (req, res, next) => {
-    return res.send('About!');
+    return res.render('about', {title: 'About', canonical: `${path}about`, year: year, bgColor: '#ffffff'});
 });
 
 module.exports = router;
